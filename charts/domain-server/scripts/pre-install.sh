36845a4d9760347ee5dcd4ecac04a23255bab615
Shuning <shuning@aukilabs.com>
2024-08-14 19:11:22 +0800

    add secret

--- charts/domain-server/scripts/pre-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install web3
-private_key="$(python3 -c "from web3 import Web3; w3 = Web3(); acc = w3.eth.account.create(); print(f'{w3.to_hex(acc.key)}')")"
-yq -i ".secrets.wallet_private_key = \"${private_key}\"" "$(dirname "$0")/../values.yaml"
