a2fab94db05a8e96064dcf2353501ff016ad393a
Oskar Levin <oskar@aukilabs.com>
2024-12-12 02:18:07 +0800

    fix: use pause image instead

--- charts/reconstruction-server/scripts/pre-install.sh
+++ charts/reconstruction-server/scripts/pre-install.sh
@@ -2,4 +2,4 @@
 
 # Set to dummy image to make tests pass as the real image isn't public yet
 
-yq -i '(.image.repository = "busybox") | (.image.tag = "latest")' "$(dirname "$0")/../values.yaml"
+yq -i '(.image.repository = "registry.k8s.io/pause") | (.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
