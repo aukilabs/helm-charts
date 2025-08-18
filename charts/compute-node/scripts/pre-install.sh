78065d323c4c34a2a20b9b3c9f864ff84c66e38a
Oskar Levin <oskar@aukilabs.com>
2025-08-18 17:34:47 +0800

    fix: tests

--- charts/compute-node/scripts/pre-install.sh
+++ charts/compute-node/scripts/pre-install.sh
@@ -2,4 +2,5 @@
 
 # Set to dummy image to make tests pass
 
-yq -i '(.image.repository = "registry.k8s.io/pause") | (.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
+yq -i '(.server.image.repository = "registry.k8s.io/pause") | (.server.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
+yq -i '(.ui.image.repository = "registry.k8s.io/pause") | (.ui.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
