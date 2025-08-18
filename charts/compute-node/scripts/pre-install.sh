d6f2358a10e75f460298f2e2c341c4939784281c
Oskar Levin <oskar@aukilabs.com>
2025-08-18 18:51:13 +0800

    fix: disable probes in tests

--- charts/compute-node/scripts/pre-install.sh
+++ charts/compute-node/scripts/pre-install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Set to dummy image to make tests pass
+# Use dummy image for now to make tests pass
 
 yq -i '(.server.image.repository = "registry.k8s.io/pause") | (.server.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
 yq -i '(.ui.image.repository = "registry.k8s.io/pause") | (.ui.image.tag = "3.10")' "$(dirname "$0")/../values.yaml"
+
+yq -i '(.server.enableProbes = false) | (.ui.enableProbes = false)' "$(dirname "$0")/../values.yaml"
